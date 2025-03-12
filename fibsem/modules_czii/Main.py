f82e0590133c9cf4d0ee8e33ef6e8267f77094af
UserHrydra <sophia.betzler@czii.org>
2025-03-12 09:30:36 -0700

    Changes from support PC

--- fibsem/modules_czii/Main.py
+++ fibsem/modules_czii/Main.py
@@ -4,16 +4,16 @@ from Fiducial_Identification import FiducialID
 from Milling import MillingSetup
 from EucentricHeight import EucentricHeight
 
-# bf = BasicFunctions(manufacturer='Thermo',
-#                 ip='192.168.0.1',
-#                 tool='Hydra',
-#                 pc_type='windows')
+bf = BasicFunctions(manufacturer='Thermo',
+                 ip='192.168.0.1',
+                 tool='Hydra',
+                 pc_type='windows')
 
-bf = BasicFunctions(
-                pc_type='mac',
-                manufacturer='Demo',
-                 ip='localhost',
-                 tool='Hydra')
+#bf = BasicFunctions(
+ #               pc_type='mac',
+#                manufacturer='Demo',
+#                 ip='localhost',
+#                 tool='Hydra')
 
 fib_microscope, fib_settings = bf.connect_to_microscope()
 imaging = Imaging(fib_microscope=fib_microscope, beam='electron', bf=bf)
@@ -24,9 +24,9 @@ imaging = Imaging(fib_microscope=fib_microscope, beam='electron', bf=bf)
 #                                        tilts=[-35.0, -25.0, -20.0, -15.0, -10.0, -5.0,
 #                                                0.0, +5.0, +10.0])
 
-
-_ = imaging.acquire_image()
-imaging.fast_acquire(200, line_acquisition=True)
+print(dir(fib_microscope.connection.SEM))
+#_ = imaging.acquire_image()
+#imaging.fast_acquire(2, line_acquisition=True)
 # parameters_dict = {
 #                      'hfw': [300.0e-6, 400.0e-6, 500.0e-6, 600.0e-6],
 #                      'current': [1e-12, 1e-12, 1e-11]

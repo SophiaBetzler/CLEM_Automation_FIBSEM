fcbb83c1c72cede884876660b618670b835ac274
Sophia Betzler <sophia.betzler@czii.org>
2025-04-21 12:27:02 -0700

    Tested to setup image acquisition.

--- fibsem/czii_scripts/Odemis_Control_Functions.py
+++ fibsem/czii_scripts/Odemis_Control_Functions.py
@@ -5,11 +5,11 @@ class OdemisControl:
 
     def __init__(self):
         odemis_microscope.add_odemis_path()
-        from odemis import model
+        from odemis import model, acq
         from odemis.acq.stream import FIBStream, SEMStream
         from odemis.util.dataio import open_acquisition
-
         self.model = model
+        self.acq = acq
         self.FIBStream = FIBStream
         self.SEMStream = SEMStream
         self.open_acquisition = open_acquisition
@@ -28,13 +28,25 @@ class OdemisControl:
         print(self.focuser.position.value)
 
     def acquire_FL_image(self):
-        acq_params = {
-            'exposure_time': 0.05,  # in seconds
-            'binning': (2, 2),  # horizontal, vertical
-        }
-        with self.open_acquisition(self.camera, mode='camera') as acq:
-            image = acq.getFrame()
-            print(f"Image shape: {image.shape}")
+
+        stream = self.acq.stream.OpticalStream()
+
+        stream.detector.value = self.camera  # Link the hardware
+        stream.tint.value = 0.1  # Integration time in seconds
+        stream.excitation.value = "DAPI"  # Or appropriate enum/string
+        stream.emission.value = "blue"
+
+        root = self.model.getRootComponent()
+        all_components = set(self.model.getHwComponents(root))
+        obs = self.acq.acqmng.SettingsObserver(microscope=root, components=all_components)
+
+        future = self.acq.acqmng.acquire([stream], settings_obs=obs)
+
+        data, err = future.result()
+        if err:
+            print("Error during acquisition:", err)
+        else:
+            print("Image acquired. Data shape:", data[0].shape)
 
     def load_tif_as_array(self, path):
         image_tif = tifffile.TiffFile(path)
